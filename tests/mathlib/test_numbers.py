52638b92305d66d392e67398143102687339d409
Steph Papanik <spapanik21@gmail.com>
2019-04-29 11:56:58 +0100

    Use lists in parametrised tests
    Both lists and tuples work, but it seems reasonable to be consistent.

--- tests/mathlib/test_numbers.py
+++ tests/mathlib/test_numbers.py
@@ -12,15 +12,15 @@ def test_lcm(a, b, expected):
 
 @pytest.mark.parametrize(
     ["n", "expected"],
-    (
-        (0, 1),
-        (1, 1),
-        (2, 2),
-        (-11, 144),
-        (-10, -89),
-        (25, 121393),
-        (40, 165580141),
-    ),
+    [
+        [0, 1],
+        [1, 1],
+        [2, 2],
+        [-11, 144],
+        [-10, -89],
+        [25, 121393],
+        [40, 165580141],
+    ],
 )
 def test_fibonacci(n, expected):
     assert numbers.fibonacci(n) == expected
--- tests/mathlib/test_primes.py
+++ tests/mathlib/test_primes.py
@@ -19,18 +19,18 @@ def test_sieve_two_arguments(upper_bound, prime_numbers):
     assert list(primes.sieve(upper_bound)) == prime_numbers
 
 
-@pytest.mark.parametrize("number", (2, 3, 5, 41, 97, 593441861))
+@pytest.mark.parametrize("number", [2, 3, 5, 41, 97, 593441861])
 def test_is_prime(number):
     assert primes.is_prime(number) is True
 
 
-@pytest.mark.parametrize("number", (1, 4, 6, 25, 3177070365797955661914307))
+@pytest.mark.parametrize("number", [1, 4, 6, 25, 3177070365797955661914307])
 def test_is_not_prime(number):
     assert primes.is_prime(number) is False
 
 
 @pytest.mark.parametrize(
-    ("number", "prime"), ((1, 2), (2, 3), (3, 5), (91, 97), (1000, 1009))
+    ["number", "prime"], [[1, 2], [2, 3], [3, 5], [91, 97], [1000, 1009]]
 )
 def test_next_prime(number, prime):
     assert primes.next_prime(number) == prime

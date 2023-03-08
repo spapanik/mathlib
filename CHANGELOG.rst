028fb9062ca53e49d5c0bc10c74948f288cbd5e6
Stephanos Kuma <stephanos@kuma.ai>
2023-03-08 17:38:09 +0000

    Add class with Collatz conjecture related methods

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -11,7 +11,8 @@ The format is based on `Keep a Changelog`_, and this project adheres to `Semanti
 
 Added
 ^^^^^
-* Added method to return all the pythagorean triplets.
+* Added method to return all the pythagorean triplets
+* Added class with Collatz conjecture methods
 
 Removed
 ^^^^^^^
---
+++ src/mathlib/general.py
@@ -0,0 +1,30 @@
+class Collatz:
+    __slots__ = ["_upper_bound", "_steps"]
+
+    def __init__(self, upper_bound: int):
+        self._upper_bound = upper_bound
+        self._steps = [0] * (upper_bound + 1)
+        self._steps[1] = 1
+
+    def __getitem__(self, item: int) -> int:
+        if item == 0:
+            return 0
+
+        if self._steps[item] == 0:
+            self._update_value(item)
+
+        return self._steps[item]
+
+    def _update_value(self, item: int) -> None:
+        if self._steps[item] != 0:
+            return
+
+        more_steps = 1
+        next_key = 3 * item + 1 if item & 1 else item >> 1
+
+        while next_key > self._upper_bound:
+            next_key = 3 * next_key + 1 if next_key & 1 else next_key >> 1
+            more_steps += 1
+
+        self._update_value(next_key)
+        self._steps[item] = self._steps[next_key] + more_steps

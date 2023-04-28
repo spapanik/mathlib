b60b43d76fd1e1522c15df83896661a1b621e23f
Stephanos Kuma <stephanos@kuma.ai>
2023-04-28 16:32:15 +0100

    Don't use an upper limit for Collatz

--- src/mathlib/general.py
+++ src/mathlib/general.py
@@ -2,32 +2,27 @@ from __future__ import annotations
 
 
 class Collatz:
-    __slots__ = ["_upper_bound", "_steps"]
+    __slots__ = ["_steps"]
 
-    def __init__(self, upper_bound: int):
-        self._upper_bound = upper_bound
-        self._steps = [0] * (upper_bound + 1)
-        self._steps[1] = 1
+    def __init__(self) -> None:
+        self._steps: dict[int, int] = {1: 1}
+
+    def __call__(self, item: int) -> int:
+        return 3 * item + 1 if item & 1 else item >> 1
 
     def __getitem__(self, item: int) -> int:
-        if item == 0:
-            return 0
+        if item <= 0:
+            raise ValueError("item must be a positive integer")
 
-        if self._steps[item] == 0:
+        if self._steps.get(item) is None:
             self._update_value(item)
 
         return self._steps[item]
 
     def _update_value(self, item: int) -> None:
-        if self._steps[item] != 0:
+        if self._steps.get(item):
             return
 
-        more_steps = 1
-        next_key = 3 * item + 1 if item & 1 else item >> 1
-
-        while next_key > self._upper_bound:
-            next_key = 3 * next_key + 1 if next_key & 1 else next_key >> 1
-            more_steps += 1
-
+        next_key = self(item)
         self._update_value(next_key)
-        self._steps[item] = self._steps[next_key] + more_steps
+        self._steps[item] = self._steps[next_key] + 1
